bbdb7eda2af07e42b73a7c18a4e0d6cfefc988bd
Hoangvmph13792 <93896285+Hoangvm02@users.noreply.github.com>
2022-12-21 17:12:34 +0700

    update_dropdown

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,3 +26,56 @@ $(document).ready(function () {
     ],
   });
 });
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".let-us-introduce-ourselves-click-dropbtn")) {
+    var dropdowns = document.getElementsByClassName(
+      "let-us-introduce-ourselves-content"
+    );
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function myTeam() {
+  var select_item = document.getElementsByClassName("meet-our-team-click-container")[0].style.display;
+  if(select_item === 'none')
+    document.getElementsByClassName("meet-our-team-click-container")[0].style.display="block";
+  else
+    document.getElementsByClassName("meet-our-team-click-container")[0].style.display="none";
+}
+function myTeamTrue() {
+  var select_itemTrue = document.getElementsByClassName("meet-our-team-click-content-true")[0].style.display;
+  if(select_itemTrue === 'none')
+    document.getElementsByClassName("meet-our-team-click-content-true")[0].style.display="block";
+  else
+    document.getElementsByClassName("meet-our-team-click-content-true")[0].style.display="none";
+
+  
+    // window.onclick = function (event) {
+    //   if (!event.target.matches(".meet-our-team-click-dropbtnTrue")) {
+    //     var dropdownsTrue = document.getElementsByClassName(
+    //       "meet-our-team-click"
+    //     );
+    //     var y;
+    //     for (y = 0; y < dropdownsTrue.length; y++) {
+    //       var openDropdownTrue = dropdownsTrue[y];
+    //       if (openDropdownTrue.classList.contains("showTrue")) {
+    //         openDropdownTrue.classList.remove("showTrue");
+    //       }
+    //     }
+    //   }
+    // };
+}
+
+
+
